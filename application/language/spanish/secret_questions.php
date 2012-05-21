bddb0b379ab7e21794bd93604d44904453d12d3d
Chazy Chaz <ChazyTheBest@hotmail.es>
2012-05-21 11:07:47 +0200

    Core/Languages New fixes and translations: * Finished messages translations. * Fixed secret_questions. * Also removed zip file.

--- application/language/spanish/secret_questions.php
+++ application/language/spanish/secret_questions.php
@@ -1,9 +1,9 @@
-<?php
+ï»¿<?php
 return array(
-    0 => "Nombre de tu mejor amigo de la infancia",
-    1 => "¿Cual es el nombre de soltera de tu madre?",
-    2 => "¿Cual es el segundo nombre de tu padre?",
-    3 => "¿Cual fue el nombre de tu primera mascota?",
-    4 => "¿A que instituto fuiste en 1er grado?",
-    5 => "¿Cual es tu pelicula favorita?"
+    0 => "Â¿Cual es el nombre de tu mejor amigo de la infancia?",
+    1 => "Â¿Cual es el nombre de soltera de tu madre?",
+    2 => "Â¿Cual es el segundo nombre de tu padre?",
+    3 => "Â¿Cual fue el nombre de tu primera mascota?",
+    4 => "Â¿A que instituto fuiste en 1er grado?",
+    5 => "Â¿Cual es tu pelicula favorita?"
 );
