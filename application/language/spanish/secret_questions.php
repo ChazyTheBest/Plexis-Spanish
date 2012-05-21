9bb5bdc9be6a7846a90460dfcafdf71bb9b297b3
Chazy Chaz <ChazyTheBest@hotmail.es>
2012-05-21 04:29:53 +0200

    Add spanish folder as a new language - more work comming soon ;)

---
+++ application/language/spanish/secret_questions.php
@@ -0,0 +1,9 @@
+﻿<?php
+return array(
+    0 => "Nombre de tu mejor amigo de la infancia.",
+    1 => "¿Cual es el nombre de soltera de tu madre?",
+    2 => "¿Cual es el segundo nombre de tu padre?",
+    3 => "¿Cual fue el nombre de tu primera mascota?",
+    4 => "¿A que instituto fuiste en 1er grado?",
+    5 => "¿Cual es tu pelicula favorita?"
+);
